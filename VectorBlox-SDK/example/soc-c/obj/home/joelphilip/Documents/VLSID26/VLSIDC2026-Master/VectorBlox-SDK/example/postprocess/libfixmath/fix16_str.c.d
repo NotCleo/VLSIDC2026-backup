6289b0645fbce48b013ec7d7f9db77dae48a93fe
joeldanphilip <joeldanphilip@gmail.com>
2025-12-06 12:06:19 +0530

    with threaded-main

---
+++ VectorBlox-SDK/example/soc-c/obj/home/joelphilip/Documents/VLSID26/VLSIDC2026-Master/VectorBlox-SDK/example/postprocess/libfixmath/fix16_str.c.d
@@ -0,0 +1,27 @@
+obj//home/joelphilip/Documents/VLSID26/VLSIDC2026-Master/VectorBlox-SDK/example/postprocess/libfixmath/fix16_str.c.o: \
+ /home/joelphilip/Documents/VLSID26/VLSIDC2026-Master/VectorBlox-SDK/example/postprocess/libfixmath/fix16_str.c \
+ /usr/riscv64-linux-gnu/include/stdc-predef.h \
+ /home/joelphilip/Documents/VLSID26/VLSIDC2026-Master/VectorBlox-SDK/example/postprocess/libfixmath/fix16.h \
+ /usr/lib/gcc-cross/riscv64-linux-gnu/11/include/stdint.h \
+ /usr/riscv64-linux-gnu/include/stdint.h \
+ /usr/riscv64-linux-gnu/include/bits/libc-header-start.h \
+ /usr/riscv64-linux-gnu/include/features.h \
+ /usr/riscv64-linux-gnu/include/features-time64.h \
+ /usr/riscv64-linux-gnu/include/bits/wordsize.h \
+ /usr/riscv64-linux-gnu/include/bits/timesize.h \
+ /usr/riscv64-linux-gnu/include/sys/cdefs.h \
+ /usr/riscv64-linux-gnu/include/bits/long-double.h \
+ /usr/riscv64-linux-gnu/include/gnu/stubs.h \
+ /usr/riscv64-linux-gnu/include/gnu/stubs-lp64d.h \
+ /usr/riscv64-linux-gnu/include/bits/types.h \
+ /usr/riscv64-linux-gnu/include/bits/typesizes.h \
+ /usr/riscv64-linux-gnu/include/bits/time64.h \
+ /usr/riscv64-linux-gnu/include/bits/wchar.h \
+ /usr/riscv64-linux-gnu/include/bits/stdint-intn.h \
+ /usr/riscv64-linux-gnu/include/bits/stdint-uintn.h \
+ /usr/lib/gcc-cross/riscv64-linux-gnu/11/include/stdbool.h \
+ /usr/riscv64-linux-gnu/include/ctype.h \
+ /usr/riscv64-linux-gnu/include/bits/endian.h \
+ /usr/riscv64-linux-gnu/include/bits/endianness.h \
+ /usr/riscv64-linux-gnu/include/bits/types/locale_t.h \
+ /usr/riscv64-linux-gnu/include/bits/types/__locale_t.h
